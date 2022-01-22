65391670392437d2cafb6dcd4f707c13425ef2b9
Parmpal <you@example.comParmpalSinghGill>
2022-01-22 18:54:33 +0530

    first commit

---
+++ appDeepSpeech.js
@@ -0,0 +1,216 @@
+var express = require('express');
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+var wav = require('wav');
+const DeepSpeech = require('deepspeech');
+const VAD = require('node-vad');
+const Speaker = require('speaker');
+
+var port = 3700;
+var outFile = 'demo.wav';
+
+let DEEPSPEECH_MODEL= '/media/parmpal/Workspace/python/Models/DeepSpeech/DeepSpeechEnglish/deepspeech-0.9.3-models';
+
+let SILENCE_THRESHOLD = 200; // how many milliseconds of inactivity before processing the audio
+
+// const VAD_MODE = VAD.Mode.NORMAL;
+// const VAD_MODE = VAD.Mode.LOW_BITRATE;
+// const VAD_MODE = VAD.Mode.AGGRESSIVE;
+const VAD_MODE = VAD.Mode.VERY_AGGRESSIVE;
+const vad = new VAD(VAD_MODE);
+
+function createModel(modelDir) {
+	let modelPath = modelDir + '.pbmm';
+	let scorerPath = modelDir + '.scorer';
+	let model = new DeepSpeech.Model(modelPath);
+	model.enableExternalScorer(scorerPath);
+	return model;
+}
+
+let englishModel = createModel(DEEPSPEECH_MODEL);
+
+let modelStream;
+let recordedChunks = 0;
+let silenceStart = null;
+let recordedAudioLength = 0;
+let endTimeout = null;
+let silenceBuffers = [];
+let firstChunkVoice = false;
+
+
+var app = express();
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', 'jade');
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.listen(port);
+
+console.log('server open on port ' + port);
+
+
+function processAudioStream(data, callback) {
+	vad.processAudio(data, 16000).then((res) => {
+		if (firstChunkVoice) {
+			firstChunkVoice = false;
+			processVoice(data);
+			return;
+		}
+		switch (res) {
+			case VAD.Event.ERROR:
+				console.log("VAD ERROR");
+				break;
+			case VAD.Event.NOISE:
+				console.log("VAD NOISE");
+				break;
+			case VAD.Event.SILENCE:
+				processSilence(data, callback);
+				break;
+			case VAD.Event.VOICE:
+				processVoice(data);
+				break;
+			default:
+				console.log('default', res);
+		}
+	});
+	// timeout after 1s of inactivity
+	clearTimeout(endTimeout);
+	endTimeout = setTimeout(function() {
+		console.log('timeout');
+		resetAudioStream();
+	},SILENCE_THRESHOLD*3);
+}
+function finishStream() {
+	if (modelStream) {
+		let start = new Date();
+		let text = modelStream.finishStream();
+		if (text) {
+			let recogTime = new Date().getTime() - start.getTime();
+			return {
+				text,
+				recogTime,
+				audioLength: Math.round(recordedAudioLength)
+			};
+		}
+	}
+	silenceBuffers = [];
+	modelStream = null;
+}
+function createStream() {
+	modelStream = englishModel.createStream();
+	recordedChunks = 0;
+	recordedAudioLength = 0;
+}
+function intermediateDecode() {
+	let results = finishStream();
+	createStream();
+	return results;
+}
+function resetAudioStream() {
+	clearTimeout(endTimeout);
+	console.log('[reset]');
+	intermediateDecode(); // ignore results
+	recordedChunks = 0;
+	silenceStart = null;
+}
+function processVoice(data) {
+	silenceStart = null;
+	if (recordedChunks === 0) {
+		console.log('');
+		process.stdout.write('[start]'); // recording started
+	}
+	else {
+		process.stdout.write('='); // still recording
+	}
+	recordedChunks++;
+	data = addBufferedSilence(data);
+	feedAudioContent(data);
+}
+function addBufferedSilence(data) {
+	let audioBuffer;
+	if (silenceBuffers.length) {
+		silenceBuffers.push(data);
+		let length = 0;
+		silenceBuffers.forEach(function (buf) {
+			length += buf.length;
+		});
+		audioBuffer = Buffer.concat(silenceBuffers, length);
+		silenceBuffers = [];
+	}
+	else audioBuffer = data;
+	return audioBuffer;
+}
+function processSilence(data, callback) {
+	if (recordedChunks > 0) { // recording is on
+		process.stdout.write('-'); // silence detected while recording
+
+		feedAudioContent(data);
+
+		if (silenceStart === null) {
+			silenceStart = new Date().getTime();
+		}
+		else {
+			let now = new Date().getTime();
+			if (now - silenceStart > SILENCE_THRESHOLD) {
+				silenceStart = null;
+				console.log('[end]');
+				let results = intermediateDecode();
+				if (results) {
+					if (callback) {
+						callback(results);
+					}
+				}
+			}
+		}
+	}
+	else {
+		process.stdout.write('.'); // silence detected while not recording
+		bufferSilence(data);
+	}
+}
+function feedAudioContent(chunk) {
+	recordedAudioLength += (chunk.length / 2) * (1 / 16000) * 1000;
+	modelStream.feedAudioContent(chunk);
+}
+function bufferSilence(data) {
+	// VAD has a tendency to cut the first bit of audio data from the start of a recording
+	// so keep a buffer of that first bit of audio and in addBufferedSilence() reattach it to the beginning of the recording
+	silenceBuffers.push(data);
+	if (silenceBuffers.length >= 3) {
+		silenceBuffers.shift();
+	}
+}
+
+
+binaryServer = BinaryServer({port: 9001});
+
+binaryServer.on('connection', function(client) {
+  console.log('new connection');
+  client.send("TESST")
+  var fileWriter = new wav.FileWriter(outFile, {
+    channels: 1,
+    sampleRate: 16000,
+    bitDepth: 16
+  });
+  createStream();
+  client.on('stream', function(stream, meta) {
+    console.log('new stream');
+    stream.pipe(fileWriter);
+//    stream.on('data', function(data){
+//      console.log('data',data);//<---Buffer type
+//    })
+    stream.on('data', function(data) {
+		processAudioStream(data,console.log);
+	});
+
+    stream.on('end', function() {
+      fileWriter.end();
+      console.log('wrote to file ' + outFile);
+    });
+  });
+});
